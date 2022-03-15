ba5452f345eb4fe378c3ef49c4c0dc1ab6783af5
Locnh1403 <100523944+Locnh1403@users.noreply.github.com>
2022-03-15 23:15:00 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,110 @@
+// **** JS String Methods ****
+// Ex 1
+// let txt = "Hello World!";
+// txt = txt.toUpperCase();
+
+// Ex 2
+// let txt = "I can eat bananas all day";
+// let x = txt.slice(10, 17);
+
+// Ex 3
+// let txt = "Hello World";
+// txt = txt.replace("Hello","Welcome");
+
+// Ex 4
+// let txt = "Hello World";
+// txt = txt.toUpperCase();
+
+// Ex 5
+// let txt = "Hello World";
+// txt = txt.toLowerCase();
+
+// Bài tập 1: Fix code
+// let a = prompt("First number?", 3);
+// let b = prompt("Second number?", 5);
+// alert(Number(a)+Number(b));
+
+// Bài tập thêm
+// **** JS Variables ****
+// Ex 1
+// var carName = "Volvo";
+
+// Ex 2
+// var x = 50;
+
+// Ex 3
+// var x = 5;
+// var y = 10;
+// document.getElementById("demo").innerHTML = x + y;
+
+// Ex 4
+// var x = 5;
+// var y = 10;
+// var z = x + y;
+// alert(z);
+
+// Ex 5
+// var firstName = "John", lastName = "Doe", age = 35;
+
+// **** JS Operator ****
+// Ex 1
+// alert(10*5);
+
+// Ex 2
+// alert(10/2);
+
+// Ex 3
+// alert(15%9);
+
+// Ex 4
+// x = 10;
+// y = 5;
+// x += y;
+
+// Ex 5
+// x = 10;
+// y = 5;
+// x *= y;
+
+// **** JS Data Types ****
+// Ex 1
+// let length = 16; // Number
+// let lastName = "Johnson"; // String
+// const x = { fistName: "John", lastName: "Doe" }; //Object
+
+// **** HOMEWORK ****
+// let a = prompt("Nhập vào phép tính: ");
+// console.log("Kết quả là: ", Number(a));
+
+// Câu 2
+// let a = parseInt(prompt("Nhập vào số thứ nhất: "));
+// let b = parseInt(prompt("Nhập vào số thứ hai: "));
+// let tong = a + b;
+// let hieu = a - b;
+// let tich = a * b;
+// let thuong = a / b;
+// console.log("Tổng hai số " + a + " và " + b + " là: ", tong);
+// console.log("Hiệu hai số " + a + " và " + b + " là: ", hieu);
+// console.log("Tích hai số " + a + " và " + b + " là: ", tich);
+// console.log("Thương hai số " + a + " và " + b + " là: ", thuong);
+
+// Chẵn lẻ
+// let a = parseInt(prompt("Nhập vào 1 số: "));
+// if (a % 2 == 0) {
+//     console.log("Số " + a + " là số chẵn");
+// } else {
+//     console.log("Số " + a + " là số lẻ");
+// }
+
+//Số nguyên tố
+// let a = parseInt(prompt("Nhập vào 1 số: "));
+// if (a < 2) { console.log("Số nguyên tố phải lớn hơn 1. Xin vui lòng thử lại!"); }
+// else {
+//     for (i = 2; i < a - 1; i++)
+//         if (a % i == 0) { console.log("Số " + a + " không phải là số nguyên tố"); break;}
+//         else { console.log("Số " + a + " là số nguyên tố"); break}
+// }
+
+
+
+
